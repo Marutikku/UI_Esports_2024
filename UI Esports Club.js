96fff3e8162cffc442d0fb2d6fc472e29337700f
Rafael Pandu Amartya <72286819+Marutikku@users.noreply.github.com>
2025-03-09 06:49:55 +0700

    Add files via upload

---
+++ UI Esports Club.js
@@ -0,0 +1,8 @@
+const observer = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		if(entry.isIntersecting)
+			document.querySelectorAll(".animated")[0].classList.add("fadeInLeft"); 
+	})
+})
+
+observer.observe(document.querySelector());
